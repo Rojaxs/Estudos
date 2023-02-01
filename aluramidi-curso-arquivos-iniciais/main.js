c45ea956d94d1290fdc44fcb8039b5e0ae6fab55
Rojaxs <123406768+Rojaxs@users.noreply.github.com>
2023-02-01 10:52:30 -0300

    Add files via upload

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,16 @@
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < ListaDeTeclas.length; contador++) {
+
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`   //template string utilizando o $ com {}
+
+    tecla.onclick = function() {
+        tocaSom(idAudio)
+    }
+}
